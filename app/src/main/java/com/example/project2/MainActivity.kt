cc33f9178275a1db925f136c3a95935a16f21592
Mikkel Kibsgaard Dall <mdall24@student.aau.dk>
2026-04-15 11:08:36 +0200

    upd

--- app/src/main/java/com/example/project2/MainActivity.kt
+++ app/src/main/java/com/example/project2/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     Project2Theme {
-        Greeting("Android")
+        Greeting("Hello")
     }
 }
