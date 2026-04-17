e1255c2fe55df4c79c1b1d4d097d625d2499c42c
Mikkel Kibsgaard Dall <mdall24@student.aau.dk>
2026-04-17 12:03:54 +0200

    Fixed errors
    Random errors fixed (supposedly)

--- app/src/main/java/com/example/p2/MainActivity.java
+++ app/src/main/java/com/example/p2/MainActivity.java
@@ -8,6 +8,8 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.p2.R;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
