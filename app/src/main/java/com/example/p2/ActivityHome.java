05d83a89296e4a0b2285cc554c8f05495af89975
Oleersej <ollibb1234@gmail.com>
2026-04-21 13:44:08 +0200

    Home screen layout, no funtion
    har hurtigt lavet layout til homescreen hvor vi nemt kan lave det til knapper

---
+++ app/src/main/java/com/example/p2/ActivityHome.java
@@ -0,0 +1,24 @@
+package com.example.p2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ActivityHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        // Pushes content down below the status bar (clock/camera)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.scrollView), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(0, systemBars.top, 0, 0);
+            return insets;
+        });
+    }
+}
