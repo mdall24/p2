b205b0190749c580892abcdabc0dc1b4b0db4f75
Mikkel Kibsgaard Dall <180258052+mdall24@users.noreply.github.com>
2026-04-28 11:17:12 +0200

    updated Stats
    Added "Previous Time" button to stats and fixed scrollView

--- app/src/main/java/com/example/p2/statistics.java
+++ app/src/main/java/com/example/p2/statistics.java
@@ -18,9 +18,12 @@ public class statistics extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_statistics);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.scrollView), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(0, systemBars.top, 0, 0);
             return insets;
         });
     }
