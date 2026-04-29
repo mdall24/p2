aff5c42469d07f380d4c258af9ca9c9745056d6a
Mette <mette.lyngholm@gmail.com>
2026-04-29 11:44:52 +0200

    Create team app picker fixed

--- app/src/main/java/com/example/p2/CreateTeam.java
+++ app/src/main/java/com/example/p2/CreateTeam.java
@@ -1,9 +1,11 @@
 package com.example.p2;
 
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.content.pm.ResolveInfo;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -33,6 +35,7 @@ public class CreateTeam extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
             });
+
         findViewById(R.id.tvNavHome).setOnClickListener(v -> {
             Intent intent = new Intent(CreateTeam.this, ActivityHome.class);
             startActivity(intent);
@@ -46,16 +49,29 @@ public class CreateTeam extends AppCompatActivity {
             startActivity(intent);
         });
 
-        RecyclerView recycler = findViewById(R.id.appRecycler);
-        recycler.setLayoutManager(new LinearLayoutManager(this));
-
-        List<AppInfo> apps = getInstalledApps();
-        appAdapter = new AppAdapter(apps);
-        recycler.setAdapter(appAdapter);
-
         Button sendInvite = findViewById(R.id.SendInvite);
         FloatingActionButton suggestTimeBtn = findViewById(R.id.SuggestedTimeButton);
         FloatingActionButton addAppsBtn = findViewById(R.id.AddAppButton);
+
+        addAppsBtn.setOnClickListener(v-> {
+            addAppsBtn.setVisibility(View.GONE);
+            suggestTimeBtn.setVisibility(View.GONE);
+
+            Intent intent = new Intent(CreateTeam.this, AppPicker.class);
+            intent.putExtra("teamcode", "ABC123");
+            startActivity(intent);
+        });
+
+        suggestTimeBtn.setOnClickListener(v ->{
+
+            TimePickerDialog timePicker = new TimePickerDialog(
+                    CreateTeam.this, (view, hourOfDay, minute) -> {
+                        int totalMinutes = hourOfDay * 60 + minute;
+                    },
+                    0, 0, true
+                    );
+            timePicker.show();
+        });
 
         sendInvite.setOnClickListener(View -> {
             suggestTimeBtn.hide();
@@ -80,30 +96,4 @@ public class CreateTeam extends AppCompatActivity {
             startActivity(Intent.createChooser(intent, "Send invite via"));
         });
     }
-    @Override
-    protected void onResume(){
-        super.onResume();
-        FloatingActionButton suggestTimeBtn = findViewById(R.id.SuggestedTimeButton);
-        FloatingActionButton addAppsBtn = findViewById(R.id.AddAppButton);
-        suggestTimeBtn.show();
-        addAppsBtn.show();
-    }
-
-    private List<AppInfo> getInstalledApps() {
-        List<AppInfo> appList = new ArrayList<>();
-
-        Intent intent = new Intent(Intent.ACTION_MAIN, null);
-        intent.addCategory(Intent.CATEGORY_LAUNCHER);
-
-        List<ResolveInfo> resolveInfos = getPackageManager().queryIntentActivities(intent, 0);
-
-        for (ResolveInfo info : resolveInfos) {
-            String appName = info.loadLabel(getPackageManager()).toString();
-            String packageName = info.activityInfo.packageName;
-            Drawable icon = info.loadIcon(getPackageManager());
-
-            appList.add(new AppInfo(appName, packageName, icon));
-        }
-        return appList;
-    }
-   }
+}
