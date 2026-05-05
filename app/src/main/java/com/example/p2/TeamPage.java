448b9e32424f37c6cdb7e18a8e2356f590e9ca11
Mikkel Kibsgaard Dall <180258052+mdall24@users.noreply.github.com>
2026-05-05 12:04:53 +0200

    Update TeamPage.java

--- app/src/main/java/com/example/p2/TeamPage.java
+++ app/src/main/java/com/example/p2/TeamPage.java
@@ -46,9 +46,9 @@ public class TeamPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_team_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bottomNav), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            v.setPadding(0, 0, 0, systemBars.bottom);
             return insets;
         });
         findViewById(R.id.tvNavHome).setOnClickListener(v -> {
